2ec712b7f833bf5272c3be39002d79d14324dc7b
Vlad Panasiuk <vlad91777@gmail.com>
2016-02-11 16:14:46 +0200

    first commit

---
+++ cv.js
@@ -0,0 +1,72 @@
+
+ $(function() {
+  $('#trigger1').click(function() {
+   if( $("ul#1").is(":hidden") ) {
+    $("ul#1").show();
+   }
+   else
+   {
+    $("ul#1").hide();
+   }
+   });
+  });
+  
+   $(function() {
+  $('#trigger2').click(function() {
+   if( $("p#2").is(":hidden") ) {
+    $("p#2").show();
+   }
+   else
+   {
+    $("p#2").hide();
+   }
+   });
+  });
+  
+   $(function() {
+  $('#trigger3').click(function() {
+   if( $("ul#3").is(":hidden") ) {
+    $("ul#3").show();
+   }
+   else
+   {
+    $("ul#3").hide();
+   }
+   });
+  });
+  
+   $(function() {
+  $('#trigger4').click(function() {
+   if( $("ul#4").is(":hidden") ) {
+    $("ul#4").show();
+   }
+   else
+   {
+    $("ul#4").hide();
+   }
+   });
+  });
+  
+   $(function() {
+  $('#trigger5').click(function() {
+   if( $("ul#5").is(":hidden") ) {
+    $("ul#5").show();
+   }
+   else
+   {
+    $("ul#5").hide();
+   }
+   });
+  });
+  
+   $(function() {
+  $('#trigger6').click(function() {
+   if( $("ul#6").is(":hidden") ) {
+    $("ul#6").show();
+   }
+   else
+   {
+    $("ul#6").hide();
+   }
+   });
+  });
